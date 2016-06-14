999386db7a33ef29b93269bccd9c38882ddda210
Scott Cho <scottsgcho@gmail.com>
2016-06-14 05:28:47 -0400

    Initial Commit

---
+++ Keyroulette/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Keyroulette
+//
+//  Created by Scott Cho on 6/14/16.
+//  Copyright © 2016 Scott Cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Keyroulette/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Keyroulette
+//
+//  Created by Scott Cho on 6/14/16.
+//  Copyright © 2016 Scott Cho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
